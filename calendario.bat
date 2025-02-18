174228ae74ba7095964b5fe26c9372ed0afebe48
LucasGrosaTakano <takanolucas44@gmail.com>
2025-02-18 16:04:57 -0300

    Update calendario.bat

--- calendario.bat
+++ calendario.bat
@@ -1,19 +1,22 @@
 @ECHO off
-
+:: se a pasta nomeada com o numero digitado não existir, ela será criada (ano)
 if not exist "%1" (
     mkdir "%1"
 ) 
 cd "%1"
+:: se a pasta nomeada com o numero digitado não existir, ela será criada (mês)
 if not exist "%2" (
     mkdir "%2"
 )
 cd "%2"
 
+::definições do ano bissexto, o comando %% é divisão
 set /a ano=%1
 set /a bissexto=%ano% %% 4
 set /a seculo=%ano% %% 100
 set /a quadricentenario=%ano% %% 400
 
+:: definições de quantos dias devem ter o mês escolhido
 set mes=%2
 if %mes%==1 set dias=31
 if %mes%==2 set dias=28 
@@ -40,14 +43,12 @@ if %mes%==11 set dias=30
 if %mes%==12 set dias=31
 
 
-
+:: cria as pastas dias na pasta mês usando um comando que faz um loop até chegar no valor de %dias%
 for /L %%i in (1,1,%dias%) do (
     if not exist %%i (
         mkdir  %%i
     )
 )
-
-
 
 
 cd ..
